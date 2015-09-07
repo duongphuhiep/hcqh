071886090dba1f8ba7c04005a263f1f7ce8419a0
duongphuhiep <duongphuhiep@gmail.com>
2015-09-07 02:37:05 +0200

    login with google open Id (https://developers.google.com/identity/sign-in/web/backend-auth)

---
+++ admin/checklogin.php
@@ -0,0 +1,25 @@
+<?php
+require_once("lib/ChromePhp.php");
+
+function checkLogin($idtoken)
+{
+    $ch = curl_init("https://www.googleapis.com/oauth2/v3/tokeninfo");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array("id_token" => $idtoken)));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //otherwise, the curl_exec will forward the page to googleapis.com
+    $dataString = curl_exec($ch);
+    curl_close($ch);
+    $data = json_decode($dataString, true);
+    //var_dump($data[array("aud")]);
+    if ($data["aud"] == "786362358731-q3s0lph8krhk90sc2bp1eujokfjbburt.apps.googleusercontent.com") {
+        return $data;
+    }
+    return null;
+}
+
+$idtoken = $_POST["idtoken"];
+$data = checkLogin($idtoken);
+
+$googleUserId = $data["sub"];
+$email = $data["email"];
+echo ((string)$googleUserId) . " (". $email . ")";
