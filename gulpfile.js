6a9aba78bb784958c985a4305f61d95949b026ca
duongphuhiep <duongphuhiep@gmail.com>
2015-10-07 19:38:30 +0200

    admin packaging

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 /**
+ * # Main application
+ *
  * - the 'gen' package is created by compiling the source files (riot tag or typescript)
  * - the 'dist' package is created by concatenate everything in the 'gen' package
  * - the 'prod' package is created by minify the 'dist' package
@@ -8,11 +10,22 @@
  * _gen, _dist, _prod
  *
  * Developper can test both the prod package or dist package with 'watch_prod' task
- *   navigate http://localhost:8080/
- *   navigate http://localhost:8080/_prod
+ *   navigate to http://localhost:8080/
+ *   navigate to http://localhost:8080/_prod
  *
  * Developper can test only the dev package with 'watch' task
  *
+ * # Admin application
+ *
+ * The admin app uses Polymer, the polybuild tools do all the job so it is much simpler,
+ * polybuild will do everything and export to `gen` package, we have to copy it to the prod package
+ * with some small modification.
+ *
+ * Developper can test the app directly:
+ *    navigate to http://localhost:8080/admin
+ *
+ * Run 'prod:admin' task to create the prod package. To test it:
+ *    navigate to http://localhost:8080/_prod/admin
  *
  * @type {Gulp|exports|module.exports}
  */
@@ -93,7 +106,11 @@ gulp.task('_minify_html_css', function() {
 		.pipe(gulp.dest('_prod'));
 });
 gulp.task('_copy_backend_php', function() {
-	gulp.src(['backend/**/*'])
+	return gulp.src(['backend/**/*'])
+		.pipe($.copy('_prod'));
+});
+gulp.task('_copy_content_to_prod', function() {
+	return 	gulp.src(['content/**/*'])
 		.pipe($.copy('_prod'));
 });
 
@@ -103,10 +120,7 @@ gulp.task('_copy_backend_php', function() {
 gulp.task('prod', ['bundle'], function(cb) {
 	runSequence(['_minify_dist_js', '_minify_html_css', '_copy_backend_php'], cb);
 });
-gulp.task('prod_content', ['prod'], function() {
-	gulp.src(['content/**/*'])
-		.pipe($.copy('_prod'));
-});
+gulp.task('prod_content', ['prod', '_copy_content_to_prod']);
 
 gulp.task('watch', ['bundle'], function(){
     gulp.watch(['app/**/*', 'lib/**/*', 'backend_mock/**/*'], ['bundle']);
@@ -126,6 +140,40 @@ gulp.task('default', ['bundle'], function(){
 
 
 
+// Polybuild will take care of inlining HTML imports,
+// scripts and CSS for you.
+var polybuild = require('polybuild');
+
+gulp.task('gen:admin', function () {
+	gulp.src('admin/index.html')
+		.pipe(polybuild({maximumCrush: true}))
+		.pipe(gulp.dest('_gen/admin'));
+
+	//rename it to prod
+	gulp.src('_gen/admin/index.build.html')
+		.pipe($.rename('index.html'))
+		.pipe(gulp.dest('_prod/admin'));
+});
+
+function replaceAdminRootFolder(rootFolder) {
+	gulp.src('_gen/admin/index.build.js')
+		.pipe($.replace('page.rootFolder="/hcqh/"', 'page.rootFolder="'+rootFolder+'"'))
+		.pipe(gulp.dest('_prod/admin'));
+}
+
+gulp.task('prod:admin', ['gen:admin', '_copy_backend_php'], function () {
+	var rootFolder = "/v3/";
+	replaceAdminRootFolder(rootFolder)
+});
+
+gulp.task('prod:admin:local', ['gen:admin', '_copy_backend_php','_copy_content_to_prod'], function () {
+	var rootFolder = "/hcqh/_prod/";
+	replaceAdminRootFolder(rootFolder)
+});
+gulp.task('watch:admin', ['prod:admin:local'], function () {
+	gulp.watch(['admin/**/*', 'backend_mock/**/*', 'lib/**/*'], ['bundle:admin']);
+});
+
 
 
 
@@ -143,70 +191,62 @@ gulp.task('reload:admin', function () {
 });
 */
 // Watch files for changes & reload
-gulp.task('watch:admin', ['bundle:admin'], function () {
-	/*browserSync({
-		port: 8080,
-		notify: false,
-		logPrefix: 'HCQH',
-		snippetOptions: {
-			rule: {
-				match: '<span id="browser-sync-binding"></span>',
-				fn: function (snippet) {
-					return snippet;
-				}
-			}
-		},
-		// Run as an https by uncommenting 'https: true'
-		// Note: this uses an unsigned certificate which on first access
-		//       will present a certificate warning in the browser.
-		// https: true,
-		server: {
-			//baseDir: ['admin'],
-			middleware: [ historyApiFallback() ],
-			routes: {
-				'/bower_components': 'bower_components',
-				'/dist': 'dist'
-			}
-		}
-	});*/
-
-	gulp.watch(['admin/**/*', 'backend_mock/**/*', 'lib/**/*'], ['bundle:admin']);
-	/*gulp.watch(['dist/admin*'], ['reload:admin']);*/
-
-	//liveServer.start({ignore:'app,admin,lib,backend_mock,tests,reports,gen', open:'/admin'});
-});
+//gulp.task('watch:admin', ['bundle:admin'], function () {
+//	/*browserSync({
+//		port: 8080,
+//		notify: false,
+//		logPrefix: 'HCQH',
+//		snippetOptions: {
+//			rule: {
+//				match: '<span id="browser-sync-binding"></span>',
+//				fn: function (snippet) {
+//					return snippet;
+//				}
+//			}
+//		},
+//		// Run as an https by uncommenting 'https: true'
+//		// Note: this uses an unsigned certificate which on first access
+//		//       will present a certificate warning in the browser.
+//		// https: true,
+//		server: {
+//			//baseDir: ['admin'],
+//			middleware: [ historyApiFallback() ],
+//			routes: {
+//				'/bower_components': 'bower_components',
+//				'/dist': 'dist'
+//			}
+//		}
+//	});*/
+//
+//	gulp.watch(['admin/**/*', 'backend_mock/**/*', 'lib/**/*'], ['bundle:admin']);
+//	/*gulp.watch(['dist/admin*'], ['reload:admin']);*/
+//
+//	//liveServer.start({ignore:'app,admin,lib,backend_mock,tests,reports,gen', open:'/admin'});
+//});
 
 // generate dist/*.js with browserify
-gulp.task('bundle:admin', function() {
-	// generate dist/admin.*.js
-	gulp.src('admin/admin.js')
-		.pipe($.plumber({
-			handleError: function (err) {
-				console.log(err);
-				this.emit('end');
-			}
-		}))
-		.pipe($.browserify({
-			detectGlobal:true,
-			debug : false
-		}))
-		.pipe(gulp.dest('dist'))
+//gulp.task('bundle:admin', function() {
+//	// generate dist/admin.*.js
+//	gulp.src('admin/admin.js')
+//		.pipe($.plumber({
+//			handleError: function (err) {
+//				console.log(err);
+//				this.emit('end');
+//			}
+//		}))
+//		.pipe($.browserify({
+//			detectGlobal:true,
+//			debug : false
+//		}))
+//		.pipe(gulp.dest('dist'))
+//
+//		//minify dist/admin.js to dist/admin.min.js
+//
+//		.pipe($.sourcemaps.init())
+//		.pipe($.rename('admin.min.js'))
+//		.pipe($.uglify())
+//		.pipe($.sourcemaps.write('./'))
+//		.pipe(gulp.dest('dist'));
+//});
+//
 
-		//minify dist/admin.js to dist/admin.min.js
-
-		.pipe($.sourcemaps.init())
-		.pipe($.rename('admin.min.js'))
-		.pipe($.uglify())
-		.pipe($.sourcemaps.write('./'))
-		.pipe(gulp.dest('dist'));
-});
-
-// Polybuild will take care of inlining HTML imports,
-// scripts and CSS for you.
-var polybuild = require('polybuild');
-
-gulp.task('vulcanize', function () {
-  return gulp.src('admin/index.html')
-    .pipe(polybuild({maximumCrush: true}))
-    .pipe(gulp.dest('admin'));
-});
