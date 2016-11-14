f24d972acdc7cb495e84741932b55d1325c0c2b7
duong <duongphuhiep@gmail.com>
2016-11-15 00:43:24 +0100

    display all reservation

---
+++ reservation/xem.php
@@ -0,0 +1,54 @@
+<?php
+require_once "secret.php";
+
+$admintoken = @$_GET['admintoken'];
+if ($admintoken != ADMINTOKEN) {
+	echo('Invalid token');
+	die(403);
+}
+
+$conn = new mysqli(DBHOST, DBLOGIN, DBPASS, DBNAME);
+if ($conn->connect_error) {
+	echo('Base de donées indisponible');
+	die(500);
+}
+$sql = "select * from booking";
+$r = $conn->query($sql);
+if (!$r) {
+	echo($conn->error);
+	die(500);
+}
+
+
+include_once 'header.php'?>
+
+<div class="container"><?php
+
+	echo $r->num_rows." places are reserved";
+	if ($r->num_rows > 0) {
+		echo "<table border='1'>";
+		echo "<tr>";
+		echo "<th>id</th>";
+		echo "<th>group</th>";
+		echo "<th>first name</th>";
+		echo "<th>last name</th>";
+		echo "<th>email</th>";
+		echo "<th>reservation date</th>";
+		echo "</tr>";
+		// output data of each row
+		while($row = $r->fetch_assoc()) {
+			echo "<tr>";
+			echo "<td>" . $row["id"]. "</td>";
+			echo "<td>" . $row["group"]. "</td>";
+			echo "<td>" . $row["firstname"]. "</td>";
+			echo "<td>" . $row["lastname"]. "</td>";
+			echo "<td>" . $row["email"]. "</td>";
+			echo "<td>" . $row["creation"]. "</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+	}
+
+?></div>
+
+<?php include_once 'footer.php'?>
